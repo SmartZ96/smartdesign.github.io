fcce9fcd48857ea12c921d6283ed8e08f93657a6
SmartZ96 <seansmart26@hotmail.co.uk>
2018-07-18 15:47:46 +0100

    first website upload

---
+++ js/gallery.js
@@ -0,0 +1,197 @@
+$(document).ready(function(){
+	$('#overlay, .gallery').hide();
+			
+	$(".thumbnail1").click(function(){
+		$("#overlay, .img1").fadeIn(500);
+	});
+	$(".thumbnail2").click(function(){
+		$("#overlay, .img2").fadeIn(500);
+	});
+	$(".thumbnail3").click(function(){
+		$("#overlay, .img3").fadeIn(500);
+	});
+	$(".thumbnail4").click(function(){
+		$("#overlay, .img4").fadeIn(500);
+	});
+	$(".thumbnail5").click(function(){
+		$("#overlay, .img5").fadeIn(500);
+	});
+	$(".thumbnail6").click(function(){
+		$("#overlay, .img6").fadeIn(500);
+	});
+	$(".thumbnail7").click(function(){
+		$("#overlay, .img7").fadeIn(500);
+	});
+	$(".thumbnail8").click(function(){
+		$("#overlay, .img8").fadeIn(500);
+	});
+	$(".thumbnail9").click(function(){
+		$("#overlay, .img9").fadeIn(500);
+	});
+		$(".thumbnail10").click(function(){
+		$("#overlay, .img10").fadeIn(500);
+	});
+		$(".thumbnail11").click(function(){
+		$("#overlay, .img11").fadeIn(500);
+	});
+		$(".thumbnail12").click(function(){
+		$("#overlay, .img12").fadeIn(500);
+	});
+		$(".thumbnail13").click(function(){
+		$("#overlay, .img13").fadeIn(500);
+	});
+		$(".thumbnail14").click(function(){
+		$("#overlay, .img14").fadeIn(500);
+	});
+		$(".thumbnail15").click(function(){
+		$("#overlay, .img15").fadeIn(500);
+	});
+		$(".thumbnail16").click(function(){
+		$("#overlay, .img16").fadeIn(500);
+	});
+		$(".thumbnail17").click(function(){
+		$("#overlay, .img17").fadeIn(500);
+	});
+			$(".next-img1").click(function(){
+				$(".img1").hide();
+				$(".img2").show();
+			});
+			$(".next-img2").click(function(){
+				$(".img2").hide();
+				$(".img3").show();
+			});
+			$(".next-img3").click(function(){
+				$(".img3").hide();
+				$(".img4").show();
+			});
+			$(".next-img4").click(function(){
+				$(".img4").hide();
+				$(".img5").show();
+			});
+			$(".next-img5").click(function(){
+				$(".img5").hide();
+				$(".img6").show();
+			});
+			$(".next-img6").click(function(){
+				$(".img6").hide();
+				$(".img7").show();
+			});
+			$(".next-img7").click(function(){
+				$(".img7").hide();
+				$(".img8").show();
+			});
+			$(".next-img8").click(function(){
+				$(".img8").hide();
+				$(".img9").show();
+			});
+				$(".next-img9").click(function(){
+				$(".img9").hide();
+				$(".img10").show();
+			});
+				$(".next-img10").click(function(){
+				$(".img10").hide();
+				$(".img11").show();
+			});
+					$(".next-img11").click(function(){
+				$(".img11").hide();
+				$(".img12").show();
+			});
+					$(".next-img12").click(function(){
+				$(".img12").hide();
+				$(".img13").show();
+			});
+					$(".next-img13").click(function(){
+				$(".img13").hide();
+				$(".img14").show();
+			});
+					$(".next-img14").click(function(){
+				$(".img14").hide();
+				$(".img15").show();
+			});
+					$(".next-img15").click(function(){
+				$(".img15").hide();
+				$(".img16").show();
+			});
+					$(".next-img16").click(function(){
+				$(".img16").hide();
+				$(".img17").show();
+			});
+					$(".next-img17").click(function(){
+				$(".img17").hide();
+				$(".img1").show();
+			});
+			
+					$(".prev-img1").click(function(){
+						$(".img1").hide();
+						$(".img17").show();
+					});
+					$(".prev-img17").click(function(){
+						$(".img17").hide();
+						$(".img16").show();
+					});	
+					$(".prev-img16").click(function(){
+						$(".img16").hide();
+						$(".img15").show();
+					});	
+					$(".prev-img15").click(function(){
+						$(".img15").hide();
+						$(".img14").show();
+					});	
+					$(".prev-img14").click(function(){
+						$(".img14").hide();
+						$(".img13").show();
+					});	
+					$(".prev-img13").click(function(){
+						$(".img13").hide();
+						$(".img12").show();
+					});	
+					$(".prev-img12").click(function(){
+						$(".img12").hide();
+						$(".img11").show();
+					});	
+					$(".prev-img11").click(function(){
+						$(".img11").hide();
+						$(".img10").show();
+					});	
+					$(".prev-img10").click(function(){
+						$(".img10").hide();
+						$(".img9").show();
+					});	
+					
+					$(".prev-img9").click(function(){
+						$(".img9").hide();
+						$(".img8").show();
+					});		
+					$(".prev-img8").click(function(){
+						$(".img8").hide();
+						$(".img7").show();
+					});		
+					$(".prev-img7").click(function(){
+						$(".img7").hide();
+						$(".img6").show();
+					});		
+					$(".prev-img6").click(function(){
+						$(".img6").hide();
+						$(".img5").show();
+					});		
+					$(".prev-img5").click(function(){
+						$(".img5").hide();
+						$(".img4").show();
+					});		
+					$(".prev-img4").click(function(){
+						$(".img4").hide();
+						$(".img3").show();
+					});		
+					$(".prev-img3").click(function(){
+						$(".img3").hide();
+						$(".img2").show();
+					});		
+					$(".prev-img2").click(function(){
+						$(".img2").hide();
+						$(".img1").show();
+					});	
+				
+						$("#close").click(function(){
+							$("#overlay, .gallery").fadeOut(500);
+						});
+});
