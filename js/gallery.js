82869bf5abbc728f7bf26330915f5a361eef4461
SmartZ96 <seansmart26@hotmail.co.uk>
2019-01-09 13:51:33 +0000

    3d model update

--- js/gallery.js
+++ js/gallery.js
@@ -1,63 +1,38 @@
 $(document).ready(function(){
 	$('#overlay, .gallery').hide();
 			
-	$(".thumbnail1").click(function(){
+	$(".modelthumbnail1").click(function(){
 		$("#overlay, .img1").fadeIn(500);
 	});
-	$(".thumbnail2").click(function(){
-		$("#overlay, .img2").fadeIn(500);
-	});
-	$(".thumbnail3").click(function(){
+
+	$(".modelthumbnail3").click(function(){
 		$("#overlay, .img3").fadeIn(500);
 	});
-	$(".thumbnail4").click(function(){
+	$(".modelthumbnail4").click(function(){
 		$("#overlay, .img4").fadeIn(500);
 	});
-	$(".thumbnail5").click(function(){
+	$(".modelthumbnail5").click(function(){
 		$("#overlay, .img5").fadeIn(500);
 	});
-	$(".thumbnail6").click(function(){
+	$(".modelthumbnail6").click(function(){
 		$("#overlay, .img6").fadeIn(500);
 	});
-	$(".thumbnail7").click(function(){
+	$(".modelthumbnail7").click(function(){
 		$("#overlay, .img7").fadeIn(500);
 	});
-	$(".thumbnail8").click(function(){
+	$(".modelthumbnail8").click(function(){
 		$("#overlay, .img8").fadeIn(500);
 	});
-	$(".thumbnail9").click(function(){
+	$(".modelthumbnail9").click(function(){
 		$("#overlay, .img9").fadeIn(500);
 	});
-		$(".thumbnail10").click(function(){
+		$(".modelthumbnail10").click(function(){
 		$("#overlay, .img10").fadeIn(500);
 	});
-		$(".thumbnail11").click(function(){
-		$("#overlay, .img11").fadeIn(500);
-	});
-		$(".thumbnail12").click(function(){
-		$("#overlay, .img12").fadeIn(500);
-	});
-		$(".thumbnail13").click(function(){
-		$("#overlay, .img13").fadeIn(500);
-	});
-		$(".thumbnail14").click(function(){
-		$("#overlay, .img14").fadeIn(500);
-	});
-		$(".thumbnail15").click(function(){
-		$("#overlay, .img15").fadeIn(500);
-	});
-		$(".thumbnail16").click(function(){
-		$("#overlay, .img16").fadeIn(500);
-	});
-		$(".thumbnail17").click(function(){
-		$("#overlay, .img17").fadeIn(500);
-	});
+
+		
 			$(".next-img1").click(function(){
 				$(".img1").hide();
-				$(".img2").show();
-			});
-			$(".next-img2").click(function(){
-				$(".img2").hide();
 				$(".img3").show();
 			});
 			$(".next-img3").click(function(){
@@ -84,75 +59,21 @@ $(document).ready(function(){
 				$(".img8").hide();
 				$(".img9").show();
 			});
-				$(".next-img9").click(function(){
+			$(".next-img9").click(function(){
 				$(".img9").hide();
 				$(".img10").show();
 			});
-				$(".next-img10").click(function(){
+			$(".next-img10").click(function(){
 				$(".img10").hide();
-				$(".img11").show();
-			});
-					$(".next-img11").click(function(){
-				$(".img11").hide();
-				$(".img12").show();
-			});
-					$(".next-img12").click(function(){
-				$(".img12").hide();
-				$(".img13").show();
-			});
-					$(".next-img13").click(function(){
-				$(".img13").hide();
-				$(".img14").show();
-			});
-					$(".next-img14").click(function(){
-				$(".img14").hide();
-				$(".img15").show();
-			});
-					$(".next-img15").click(function(){
-				$(".img15").hide();
-				$(".img16").show();
-			});
-					$(".next-img16").click(function(){
-				$(".img16").hide();
-				$(".img17").show();
-			});
-					$(".next-img17").click(function(){
-				$(".img17").hide();
 				$(".img1").show();
 			});
-			
+
+				
 					$(".prev-img1").click(function(){
 						$(".img1").hide();
-						$(".img17").show();
-					});
-					$(".prev-img17").click(function(){
-						$(".img17").hide();
-						$(".img16").show();
-					});	
-					$(".prev-img16").click(function(){
-						$(".img16").hide();
-						$(".img15").show();
-					});	
-					$(".prev-img15").click(function(){
-						$(".img15").hide();
-						$(".img14").show();
-					});	
-					$(".prev-img14").click(function(){
-						$(".img14").hide();
-						$(".img13").show();
-					});	
-					$(".prev-img13").click(function(){
-						$(".img13").hide();
-						$(".img12").show();
-					});	
-					$(".prev-img12").click(function(){
-						$(".img12").hide();
-						$(".img11").show();
-					});	
-					$(".prev-img11").click(function(){
-						$(".img11").hide();
 						$(".img10").show();
-					});	
+					});
+		
 					$(".prev-img10").click(function(){
 						$(".img10").hide();
 						$(".img9").show();
@@ -184,12 +105,8 @@ $(document).ready(function(){
 					});		
 					$(".prev-img3").click(function(){
 						$(".img3").hide();
-						$(".img2").show();
-					});		
-					$(".prev-img2").click(function(){
-						$(".img2").hide();
 						$(".img1").show();
-					});	
+					});		
 				
 						$("#close").click(function(){
 							$("#overlay, .gallery").fadeOut(500);
